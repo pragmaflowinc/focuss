79d13550bfd9f3a3778154c078d42ec041b0ebb4
Mitch <72631198+Mitch-PragmaFlow@users.noreply.github.com>
2022-09-21 10:21:34 -0400

    attempt at switch

--- src/components/NuToggle.jsx
+++ src/components/NuToggle.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { ToggleButton } from 'ui-neumorphism'
-import 'ui-neumorphism/dist/index.css'
+import React, { useState} from "react";
+import { ToggleButton } from "ui-neumorphism";
+import "ui-neumorphism/dist/index.css";
+
 
 
 export function NuToggle(props) {
-    const inputStyles = props.styles
+  const [checked, setChecked] = useState(false);
 
-
-      return <ToggleButton
-        
-        dark= {true}
-        height={props.height}
-        className= {props.className} //outter-most div
-        name='onTopToggle'
-        inputStyles={inputStyles}
-        margin-left= "auto"
-        margin-right= "auto"
-        onclick= {props.onclick}
-      />
-  }
-
-
+  return (
+    <ToggleButton
+      name={props.name}
+      color= {props.color}      
+      dark={true}
+      height={props.height}
+      width={props.width}
+      className='{props.className}' //outter-most div
+      // onclick={props.onclick}
+      label={props.label}
+      // disabled={props.disabled || false}
+      checked={checked || true}
+      // onChange={props.onChange}
+      onchange={(e) => { setChecked(e.target.checked) }}
+      
+      
+    />
+  );
+}
